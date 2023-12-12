88d03d9683cb72174e25e9562f80a8d5e88af104
Anatoliy Zakhryapin <anatoli.zakhryapin@gmail.com>
2023-12-12 16:33:00 +0100

    ADD app.blade.php

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>@yield('title')</title>
+
+    <!-- Fonts -->
+    <link href="https://fonts.bunny.net/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+
+    <!-- Styles -->
+    @vite('resources/js/app.js')
+
+</head>
+
+<body class="d-flex flex-column">
+
+    {{-- header --}}
+    {{-- @include('partials.header') --}}
+
+    {{-- main content --}}
+    <main class="flex-grow-1">
+        @yield('content')
+    </main>
+
+    {{-- footer --}}
+    {{-- @include('partials.footer') --}}
+
+</body>
+
+</html>
